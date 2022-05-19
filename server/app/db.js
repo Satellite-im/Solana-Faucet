977114938704fe071b378ba622e20abc0a8adb9f
Joe McGrath <josephmcg01@gmail.com>
2022-05-19 10:46:55 +0900

    fix(logic): logic error and prettier formatting

--- server/app/db.js
+++ server/app/db.js
@@ -1,110 +1,110 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const { Pool } = require("pg");
+const { Pool } = require('pg')
 connString = process.env.PG_CONN
 
 class Database {
-  databaseName = "AccessCodes"
-  accessCodesTablename = "ACCESS_CODES"
-  
-  db;
+  databaseName = 'AccessCodes'
+  accessCodesTablename = 'ACCESS_CODES'
+
+  db
 
   constructor(inputDBName) {
-    this.databaseName = inputDBName;
-    this.init();
+    this.databaseName = inputDBName
+    this.init()
   }
 
   async init() {
-    this.pool = new Pool({connectionString: connString});
+    this.pool = new Pool({ connectionString: connString })
 
     /* UNCOMMENT this section to test methods vvv */
-    await this.createTable(); // This creates the table if it doesn't exist
-    await this.insertSampleData("Hello Tom!", 10); // this inserts a new access code, with a 10 limit
-    await this.insertSampleData("RINGO123", 1); // this inserts a new access code, with a 10 limit
-    await this.insertSampleData("Cactus54321", 1); // this inserts a new access code, with a 10 limit
+    await this.createTable() // This creates the table if it doesn't exist
+    // await this.insertSampleData("Hello Tom!", 10); // this inserts a new access code, with a 10 limit
+    // await this.insertSampleData("RINGO123", 1); // this inserts a new access code, with a 10 limit
+    // await this.insertSampleData("Cactus54321", 1); // this inserts a new access code, with a 10 limit
     // await this.accessCodeIsValid("hi3333r3aaaaa"); // {status: true/false} response
 
     // this.incrementCode('hi3333r3aaaaa') // bump the code by one if it's not maxed out
     // await this.getStatus(); // array with all objects, minus the access code
     /* UNCOMMENT this section to test methods ^^^ */
 
-    return;
+    return
   }
 
   // // Adds a table with custom tablename, but hard coded schema for now
   async createTable() {
     const buildTableQuery = `CREATE TABLE IF NOT EXISTS ${this.accessCodesTablename} 
-          ( ID serial PRIMARY KEY, CODE VARCHAR(25) NOT NULL UNIQUE, MAX INTEGER DEFAULT 0 NOT NULL, USED INTEGER DEFAULT 0 NOT NULL);`;
+          ( ID serial PRIMARY KEY, CODE VARCHAR(25) NOT NULL UNIQUE, MAX INTEGER DEFAULT 0 NOT NULL, USED INTEGER DEFAULT 0 NOT NULL);`
 
     await this.pool.connect().then((client) => {
       return client
         .query(buildTableQuery)
         .then(() => {
-          client.release();
+          client.release()
         })
         .catch((err) => {
-          client.release();
-          console.log(err.stack);
-        });
-    });
+          client.release()
+          console.log(err.stack)
+        })
+    })
   }
   async insertSampleData(sampleCode, max, start = 0) {
     // string, int, int
     //   // seed sample data
     await this.pool.connect((err, client) => {
-      if (err) throw err;
+      if (err) throw err
       return client
         .query(
           `INSERT INTO ${this.accessCodesTablename} (CODE,MAX,USED) VALUES($1, $2, $3);`,
-          [sampleCode, max, start]
+          [sampleCode, max, start],
         )
         .then((res) => {
-          client.release();
+          client.release()
         })
         .catch((err) => {
-          client.release();
-          console.log(err.stack);
-        });
-    });
+          client.release()
+          console.log(err.stack)
+        })
+    })
   }
 
   async accessCodeIsValid(accessCode) {
     // connect using the pool
-    const client = await this.pool.connect();
+    const client = await this.pool.connect()
     // run the query with a sanitized access code input
     const res = await client.query(
       `SELECT * FROM ${this.accessCodesTablename} WHERE CODE = $1;`,
-      [accessCode]
-    );
+      [accessCode],
+    )
 
     // there can only be one record per access code, so get it and make sure it hasn't been used up
-    const responseRow = res.rows[0];
+    const responseRow = res.rows[0]
     if (responseRow && responseRow.max > responseRow.used) {
-      return { status: true };
+      return { status: true }
     }
-    return { status: false };
+    return { status: false }
   }
 
   // increments the used column by 1
   async incrementCode(accessCode) {
     // Checks if the codes max is greater than the used column
-    const isValid = await this.accessCodeIsValid(accessCode);
-    const client = await this.pool.connect();
+    const isValid = await this.accessCodeIsValid(accessCode)
+    const client = await this.pool.connect()
     if (isValid.status) {
       const res = await client.query(
         `UPDATE ${this.accessCodesTablename} SET USED = USED + 1 WHERE CODE = $1`,
-        [accessCode]
-      );
+        [accessCode],
+      )
     }
   }
   async getStatus() {
-    const client = await this.pool.connect();
+    const client = await this.pool.connect()
     return await client
       .query(`SELECT id, max, used FROM ${this.accessCodesTablename}`)
       .then((res) => {
-        return res.rows;
-      });
+        return res.rows
+      })
   }
 }
 
-module.exports = Database;
+module.exports = Database
--- server/index.js
+++ server/index.js
@@ -1,79 +1,79 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const web3 = require("@solana/web3.js");
-const express = require("express");
-const cors = require("cors");
+const web3 = require('@solana/web3.js')
+const express = require('express')
+const cors = require('cors')
 const Database = require('./app/db')
 
 let db = new Database('AccessCodes')
 
-const app = express();
-const port = process.env.PORT;
+const app = express()
+const port = process.env.PORT
 
-const SolanaClusters = ["devnet", "testnet", "mainnet-beta"];
+const SolanaClusters = ['devnet', 'testnet', 'mainnet-beta']
 
 //Initializing Solana network connection
 const connection = SolanaClusters.includes(process.env.SOLANA_NETWORK)
   ? new web3.Connection(
       web3.clusterApiUrl(process.env.SOLANA_NETWORK),
-      "confirmed"
+      'confirmed',
     )
-  : new web3.Connection(process.env.SOLANA_NETWORK, "confirmed");
+  : new web3.Connection(process.env.SOLANA_NETWORK, 'confirmed')
 
 //Retrieve payer account keys
 const payerAccount = web3.Keypair.fromSecretKey(
-  Buffer.from(process.env.PAYER_PRIVATE_KEY, "base64")
-);
+  Buffer.from(process.env.PAYER_PRIVATE_KEY, 'base64'),
+)
 
-console.log(`Payer: ${payerAccount.publicKey.toBase58()}`);
+console.log(`Payer: ${payerAccount.publicKey.toBase58()}`)
 
-const corsOrigin = (process.env.CORS_DOMAIN) ? process.env.CORS_DOMAIN : '*'
+const corsOrigin = process.env.CORS_DOMAIN ? process.env.CORS_DOMAIN : '*'
 app.use(
   cors({
     origin: corsOrigin,
-  })
-);
+  }),
+)
 
 // parse application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 // parse application/json
-app.use(express.json());
+app.use(express.json())
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   console.log('request received')
   return res.status(200).json({
-    status: "success",
-    message: "Service Healthy",
-  });
+    status: 'success',
+    message: 'Service Healthy',
+  })
 })
-app.post("/", async (req, res) => {
+app.post('/', async (req, res) => {
   //Retrieve public key from address, and the access code
-  const { address, accessCode } = req.body;
+  const { address, accessCode } = req.body
 
   // if they don't provide what we need, or the access code is invalid kick out here
-  if (!address || await db.accessCodeIsValid(accessCode)) {
+  if (!address || !(await db.accessCodeIsValid(accessCode))) {
     return res.status(400).json({
-      status: "failed",
+      status: 'failed',
       errorCode: 3,
-      errorMessage: "Malformed request",
-    });
+      errorMessage: 'Malformed request',
+    })
   }
 
-  if (process.env.ENVIRONMENT === 'EARLY_ACCESS' && !accessCode){
+  if (process.env.ENVIRONMENT === 'EARLY_ACCESS' && !accessCode) {
     return res.status(400).json({
-      status: "failed",
+      status: 'failed',
       errorCode: 3,
-      errorMessage: "Malformed request",
-    });
+      errorMessage: 'Malformed request',
+    })
   }
 
   // The faucet will ask to fund itself at each
   // api call
-  requestAirdrop();
+  requestAirdrop()
 
-  const to = new web3.PublicKey(address);
+  const to = new web3.PublicKey(address)
 
-  const balance = await connection.getBalance(to);
+  const balance = await connection.getBalance(to)
 
   if (balance < web3.LAMPORTS_PER_SOL * process.env.BALANCE_LIMIT) {
     try {
@@ -83,93 +83,97 @@ app.post("/", async (req, res) => {
           fromPubkey: payerAccount.publicKey,
           toPubkey: to,
           lamports: web3.LAMPORTS_PER_SOL * process.env.SOL_AMOUNT,
-        })
-      );
+        }),
+      )
 
       // Sign transaction, broadcast, and confirm
       const signature = await web3.sendAndConfirmTransaction(
         connection,
         transaction,
-        [payerAccount]
-      );
-      if (process.env.ENVIRONMENT === 'EARLY_ACCESS' && accessCode){
+        [payerAccount],
+      )
+      if (process.env.ENVIRONMENT === 'EARLY_ACCESS' && accessCode) {
         await db.incrementCode(accessCode)
       }
 
       //response for web3 transaction success
       res.json({
-        status: "success",
+        status: 'success',
         transactionSignature: signature,
-      });
+      })
     } catch (error) {
       //response for web3 transaction error
       res.json({
-        status: "failed",
+        status: 'failed',
         errorCode: 2,
         errorMessage: error,
-      });
+      })
     }
   } else {
     //Response for balance limit excedeed
     res.json({
-      status: "failed",
+      status: 'failed',
       errorCode: 1,
-      errorMessage: "The account balance limit has already been exceeded",
-    });
+      errorMessage: 'The account balance limit has already been exceeded',
+    })
   }
-});
+})
 
 // Returns an object with status on all access codes
-app.get("/checkCode/status", async (req, res) => {
+app.get('/checkCode/status', async (req, res) => {
   console.log('here')
   let allCodeStatus = await db.getStatus()
   console.log(allCodeStatus)
-  if(!allCodeStatus){
+  if (!allCodeStatus) {
     return res.status(400).json({
-      status: "failed",
+      status: 'failed',
       errorCode: 4,
-      errorMessage: "Malformed request or invalid code",
-    });
+      errorMessage: 'Malformed request or invalid code',
+    })
   }
   return res.status(200).json({
-    status: "success",
-    message: allCodeStatus
-  });
+    status: 'success',
+    message: allCodeStatus,
+  })
 })
 
 // Allows you to check a single access code for a boolean response if it's good or not
-app.get("/checkCode/:accessCode", async (req, res) => {
+app.get('/checkCode/:accessCode', async (req, res) => {
   //Retrieve public key from address
 
-  const { accessCode } = req.params;
+  const { accessCode } = req.params
   let validStatus = await db.accessCodeIsValid(accessCode)
   if (!accessCode && !validStatus.status) {
     return res.status(400).json({
-      status: "failed",
+      status: 'failed',
       errorCode: 4,
-      errorMessage: "Malformed request or invalid code",
-    });
+      errorMessage: 'Malformed request or invalid code',
+    })
   }
-  if(validStatus){
+  if (validStatus) {
     return res.status(200).json({
-      status: "success",
-      message: validStatus.status
-    });
+      status: 'success',
+      message: validStatus.status,
+    })
   }
   return res.status(400).json({
-    status: "failed",
+    status: 'failed',
     errorCode: 5,
-    errorMessage: "Unknown Error",
-  });
-});
+    errorMessage: 'Unknown Error',
+  })
+})
 
 app.listen(port, () =>
-  console.log(`Solana faucet app listening on port ${port}!`, 'ENV: ', process.env.ENVIRONMENT)
-);
+  console.log(
+    `Solana faucet app listening on port ${port}!`,
+    'ENV: ',
+    process.env.ENVIRONMENT,
+  ),
+)
 
 async function requestAirdrop() {
   return connection.requestAirdrop(
     payerAccount.publicKey,
-    web3.LAMPORTS_PER_SOL * 1
-  );
+    web3.LAMPORTS_PER_SOL * 1,
+  )
 }
